112ba01fe6e1fb89fb92abe6f8fd2b83d80d07f8
CR-0007 <dwd00b@gmail.com>
2025-12-05 00:27:18 +0000

    payment page updated
    improved the validations for each input field and changed the logo position

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const paymentForm = document.getElementById("paymentForm");
+const cardNumber = document.getElementById("cardNumber");
+const cvv = document.getElementById("cvv");
 
-paymentForm.addEventListener("submit", function (e) {
-  e.preventDefault(); 
-
-  alert("Payment successful! Thank you for your purchase.");
-
-  paymentForm.reset(); 
+cardNumber.addEventListener("input", function () {
+    this.value = this.value.slice(0, 16);
 });
 
+cvv.addEventListener("input", function () {
+    this.value = this.value.slice(0, 3);
+});
+
+
+paymentForm.addEventListener("submit", function (e) {
+
+    if (cardNumber.value.length !== 16) {
+        alert("Card number must be exactly 16 digits.");
+        e.preventDefault();
+        return;
+    }
+
+    if (cvv.value.length !== 3) {
+        alert("CVV must be exactly 3 digits.");
+        e.preventDefault();
+        return;
+    }
+
+    alert("Payment successful! Thank you for your purchase.");
+    paymentForm.reset();
+});
